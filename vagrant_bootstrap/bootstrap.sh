4d226febd12054e3e5589740a6b47d1e1e119215
welcominh <welcominh@gmail.com>
2017-03-13 12:24:07 +0100

    Add php-ldap

--- vagrant_bootstrap/bootstrap.sh
+++ vagrant_bootstrap/bootstrap.sh
@@ -24,7 +24,7 @@ sudo a2dismod php5
 echo "=========================================================="
 echo "Installing Xdebug."
 echo "=========================================================="
-yes | sudo apt-get install php php-mysql php-xml php-xdebug php-pgsql
+yes | sudo apt-get install php php-mysql php-xml php-xdebug php-pgsql php-ldap
 sudo service apache2 restart
 
 
