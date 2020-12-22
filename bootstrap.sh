b29994034752460d8e3f92d8f6e84a6e90c8fd61
welcominh <welcominh@gmail.com>
2020-12-22 14:11:30 +0100

    Add php-igbinary

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,7 @@ echo "=========================================================="
 echo "Installing PHP missing libs"
 echo "=========================================================="
 
-sudo apt-get install php7.4-dom php7.4-xml php7.4-mysql php7.4-gd php7.4-mbstring php7.4-curl php7.4-bcmath php7.4-intl php7.4-zip
+sudo apt-get install php7.4-dom php7.4-xml php7.4-mysql php7.4-gd php7.4-mbstring php7.4-curl php7.4-bcmath php7.4-intl php7.4-zip php7.4-igbinary
 sudo apt-get install rabbitmq-server
 
 echo "=========================================================="
